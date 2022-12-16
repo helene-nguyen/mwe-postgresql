3478f16205dccd62454a35692243a80c53fbf8d0
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-12-16 05:55:57 +0100

    :memo: docs : training

--- pgsql-vacuum-full-alt/mwe_vacuum_full_alt.sql
+++ pgsql-vacuum-full-alt/mwe_vacuum_full_alt.sql
@@ -13,7 +13,7 @@ CREATE DATABASE mwe_vacuum_full;
 --=======================
 -- Model
 --=======================
-DROP TABLE "category", "article", "user";
+DROP TABLE "article_history","category", "article", "user";
 
 CREATE TABLE IF NOT EXISTS "user"(
 id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -155,6 +155,42 @@ $$ LANGUAGE plpgsql;
 
 SELECT * FROM rel_trigger(array['user','article','category']::text[]);
 
+
+--================================================
+-- Create triggers
+--================================================
+
+-- DROP TRIGGER IF EXISTS article_tg
+-- ON article;
+
+-- DROP TRIGGER IF EXISTS category_tg
+-- ON category;
+
+CREATE TABLE article_history (LIKE article INCLUDING ALL);
+
+DROP FUNCTION article_tg, category_tg CASCADE;
+
+CREATE OR REPLACE FUNCTION article_tg() 
+RETURNS TRIGGER AS $article_tg$
+    BEGIN
+		-- IF (TG_OP = 'DELETE') THEN
+        --     RAISE NOTICE 'content cannot be deleted';
+        -- -- Check content is given
+        -- END IF;
+		INSERT INTO article_history (user_id, content)
+		VALUES (OLD.user_id, OLD.content);
+		RETURN OLD;
+      	
+    END;
+$article_tg$ LANGUAGE plpgsql;
+
+-- Insert in a history table for each delete row
+CREATE TRIGGER article_tg 
+AFTER DELETE ON article
+FOR EACH ROW EXECUTE FUNCTION article_tg();
+
+-- DELETE FROM "article" WHERE id = 1088;
+
 --==============================================================
 -- Create function to collect size & path information tables  
 --==============================================================
@@ -203,6 +239,24 @@ SELECT * FROM table_details('category');
 
 \x
 
+-- --=================================
+-- Scan the tables BEFORE DELETE
+-- --=================================
+
+SELECT 
+	relname, 
+	seq_scan, 
+	seq_tup_read, 
+	idx_scan,
+	n_tup_ins,
+	n_tup_upd,
+	n_tup_del, 
+	n_live_tup, 
+	n_dead_tup,
+	last_vacuum
+FROM pg_stat_user_tables 
+ORDER BY seq_scan DESC \gx
+
 --================================================
 -- Delete datas
 --================================================
@@ -210,6 +264,8 @@ SELECT * FROM table_details('category');
 DELETE FROM "category" WHERE id <= 30000;
 DELETE FROM "article" WHERE id <= 10000;
 DELETE FROM "user" WHERE id <= 10000;
+
+-- TABLE article_history;
 
 --================================================
 -- Details AFTER DELETE
@@ -225,10 +281,30 @@ SELECT * FROM table_details('category');
 
 -- Here you can check details about density of DATABASE
 
+-- --=================================
+-- Scan the tables AFTER DELETE
+-- --=================================
 
--- --=================================
+SELECT 
+	relname, 
+	seq_scan, 
+	seq_tup_read, 
+	idx_scan,
+	n_tup_ins,
+	n_tup_upd,
+	n_tup_del, 
+	n_live_tup, 
+	n_dead_tup,
+	last_vacuum,
+	last_autovacuum
+FROM pg_stat_user_tables 
+ORDER BY seq_scan DESC \gx
+
+-- here results to check are in n_dead_tup => 0 to the nb of deleted rows
+
+--=================================
 -- Add data at the end of the table
--- --=================================
+--=================================
 
 INSERT INTO "user" ("name")
 (
@@ -260,3 +336,32 @@ INSERT INTO category ("article_id", "content")
 	FROM generate_series( 1, 1000) as serie_nb
 );
 
+-- --=================================
+-- Number pages before VACUUM
+-- --=================================
+
+SELECT relname, relpages FROM pg_class WHERE relname IN ('user','article','category');
+
+-- TODO
+
+
+
+CREATE OR REPLACE FUNCTION move_rows (name TEXT) -- doesn't take parameter
+RETURNS TABLE(
+	content TEXT) AS $$
+
+DECLARE table_name TEXT := name;
+
+BEGIN
+RAISE NOTICE '"%"', table_name;
+	ALTER TABLE article DISABLE TRIGGER ALL;
+	WITH d AS (DELETE FROM table_name WHERE (ctid::text::point)[0] >= 2 RETURNING *)
+	INSERT INTO name(user_id, content) (SELECT user_id, content FROM d);
+	ALTER TABLE name ENABLE TRIGGER ALL;
+	
+
+RETURN QUERY(SELECT content FROM name LIMIT 1);
+
+END;
+
+$$ LANGUAGE plpgsql;
